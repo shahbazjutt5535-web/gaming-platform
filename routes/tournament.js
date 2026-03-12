4cd28e869c6d941a7d734921a5f155cb772faddb
shahbazjutt5535-web <shahbazjutt5535@gmail.com>
2026-03-12 20:41:21 +0500

    Update tournament.js

--- routes/tournament.js
+++ routes/tournament.js
@@ -1,13 +1,21 @@
-const express = require("express");
-const router = express.Router();
-const Tournament = require("../models/Tournament");
-
-// Create tournament
 router.post("/create", async (req, res) => {
-  const { name, game, prizePool } = req.body;
-  const t = new Tournament({ name, game, prizePool, players: [], winners: [] });
-  await t.save();
-  res.json({ message: "Tournament created", tournament: t });
-});
 
-module.exports = router;
+  const { name, prizePool, game } = req.body;
+
+  const tournament = new Tournament({
+
+    name,
+    prizePool,
+    game,
+    players: []
+
+  });
+
+  await tournament.save();
+
+  res.json({
+    message: "Tournament created",
+    tournament
+  });
+
+});
