c0d5bd8b2550679b58f54c08932e2a986a16325a
shahbazjutt5535-web <shahbazjutt5535@gmail.com>
2026-03-12 20:34:54 +0500

    Update game.js

--- routes/game.js
+++ routes/game.js
@@ -1,84 +1,16 @@
-const express = require("express");
-const router = express.Router();
+const PlatformSettings = require("../models/PlatformSettings");
 
-const User = require("../models/User");
-const GameLog = require("../models/GameLog");
+async function generateResult() {
 
-function generateResult() {
+  const settings = await PlatformSettings.findOne();
+
+  const winChance = settings ? settings.winProbability : 0.48;
 
   const random = Math.random();
 
-  if (random < 0.48) {
+  if (random < winChance) {
     return "win";
   }
 
   return "lose";
 }
-
-router.post("/play", async (req, res) => {
-
-  try {
-
-    const { playerCode, betAmount, game } = req.body;
-
-    const user = await User.findOne({ playerCode });
-
-    if (!user) {
-      return res.json({ message: "User not found" });
-    }
-
-    if (user.balance < betAmount) {
-      return res.json({
-        message: "Insufficient balance"
-      });
-    }
-
-    user.balance -= betAmount;
-
-    const result = generateResult();
-
-    let winAmount = 0;
-
-    if (result === "win") {
-
-      winAmount = betAmount * 2;
-
-      user.balance += winAmount;
-
-    }
-
-    await user.save();
-
-    const log = new GameLog({
-
-      playerCode,
-      game,
-      betAmount,
-      result,
-      winAmount,
-      balanceAfter: user.balance
-
-    });
-
-    await log.save();
-
-    res.json({
-
-      result,
-      winAmount,
-      balance: user.balance
-
-    });
-
-  } catch (err) {
-
-    res.status(500).json({
-      message: "Server error",
-      error: err.message
-    });
-
-  }
-
-});
-
-module.exports = router;
