34c90c7f88e57ef470ea76a4bbf2dfbe79844f9f
shahbazjutt5535-web <shahbazjutt5535@gmail.com>
2026-03-12 19:58:00 +0500

    Create crypto.js

---
+++ utils/crypto.js
@@ -0,0 +1,8 @@
+// Placeholder for crypto API check (TRC20, ERC20, BTC)
+async function checkDeposit(address) {
+  // Connect to blockchain API
+  // Check if new deposit > 10 USD
+  // Return deposit info
+}
+
+module.exports = { checkDeposit };
