d899bb98d12e4f13e45bb6ea79331b8299c3ad84
shahbazjutt5535-web <shahbazjutt5535@gmail.com>
2026-03-12 15:45:27 +0500

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  playerId: String,
+  balance: {
+    type: Number,
+    default: 0
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
